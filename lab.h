a22c9d5295e6625b25afedbec7bfc55c80fcab88
Yaroslav-Ryabets <ignitetoprus.228@gmail.com>
2025-04-23 22:15:27 +0300

    Update lab.h

--- lab.h
+++ lab.h
@@ -1,47 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define YES 1
+#define NO 0
+#define max_iterations 100
 
-#define YES 1 
-#define NO 0 
-
-int main()
-{
- int c;
- int first_char;
- int prev_char;
- int current_char;
- int flag;
- int cnt;
- 
- cnt = 0;
- flag = NO;
- first_char = ' ';
- prev_char = ' ';
-  while ( ( c = getchar() ) !=EOF )
-  {
-      if (c == ' '  c == '\t'  c =='\n' || c ==',')
-      {
-           if (flag == YES)
-           {
-               if (first_char == current_char)
-                 cnt = cnt + 1;
-               
-               flag = NO;
-           }
-      }
-      else
-      {
-          if (flag == NO)   
-          {
-              first_char = c;
-              flag = YES;
-          }
-           prev_char = current_char;
-           current_char = c;
-      }
-  }
- if (flag==YES && first_char == current_char)
-    cnt = cnt + 1;
-
- printf("Количество слов: %d\n", cnt);
- return 0;
-}
+void lab_1();
+void lab_1_1();
+float square (float number);
+void lab_2();
+void lab_2_1();
+void lab_3();
+void lab_4();
+void lab_5();
+void lab_6();
